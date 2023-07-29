eadf28aa166515b3cc8343f20168f257cf4037a1
Ivan Kulikov <ivankulich@gmail.com>
2023-07-29 19:09:39 +0300

    https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/

---
+++ 1378. Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
+SELECT EmployeeUNI.unique_id, Employees.name FROM
+    Employees LEFT JOIN EmployeeUNI ON
+    Employees.id = EmployeeUNI.id
